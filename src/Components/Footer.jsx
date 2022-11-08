d772e253fb6fd343dfd333900ffdf8a42d594be9
leonardo <leonardotobe94@gmail.com>
2022-11-08 18:31:00 -0400

    letter spacing

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,7 @@
 export const Footer = () => {
   return (
     <div className='w-full h-64 bg-[black] flex justify-center items-center mt-20'>
-      <small className=' text-white '>2022</small>
+      <small className=' text-white '>2 0 2 2</small>
     </div>
   )
 }
